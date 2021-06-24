71c2309cb0f70e6539d31a8fefd27524368434ed
vasanza <vasanza@espol.edu.ec>
2021-06-24 11:54:54 -0500

    Decoders

---
+++ DecBCD_7seg.vhd
@@ -0,0 +1,38 @@
+--BCD data decoder to 7 segment display
+--https://vasanza.blogspot.com
+
+--Library
+library ieee;
+use ieee.std_logic_1164.all;
+
+--Entity
+entity DecBCD_7seg is
+	port(
+		BCD: in std_logic_vector(3 downto 0);
+		anodo7: out std_logic_vector(6 downto 0));
+end DecBCD_7seg;
+
+--Architecture
+architecture solve of DecBCD_7seg is
+	-- Signals,Constants,Variables,Components
+	begin
+	--Process #1
+	process(BCD)
+	--Sequential programming
+		begin
+			case BCD is
+				when "0000" => anodo7<="1111110";
+				when "0001" => anodo7<="0110000";
+				when "0010" => anodo7<="1101101";
+				when "0011" => anodo7<="1111001";
+				when "0100" => anodo7<="0110011";
+				when "0101" => anodo7<="1011011";
+				when "0110" => anodo7<="0011111";
+				when "0111" => anodo7<="1110000";
+				when "1000" => anodo7<="1111111";
+				when "1001" => anodo7<="1110011";
+				when others => anodo7<="0000000";
+			end case;
+	end process;
+	--Process #n...
+end solve;
---
+++ DecoBin_BCD.vhd
@@ -0,0 +1,49 @@
+--8-bit Binary Data Decoder to BCD (Units, Tens and Hundreds)
+--https://vasanza.blogspot.com
+
+--Library
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+--Entity
+entity DecoBin_BCD is
+    port(
+        num_bin: in  std_logic_vector(8 downto 0);
+        num_bcd: out std_logic_vector(10 downto 0));
+end DecoBin_BCD;
+
+--Architecture
+architecture solve of DecoBin_BCD is
+	-- Signals,Constants,Variables,Components
+	begin
+    --Process #1
+	proceso_bcd: process(num_bin)
+	--Sequential programming
+	variable z: std_logic_vector(19 downto 0);
+		begin
+		-- Inicialización de datos en cero.
+		z := (others => '0');
+		-- Se realizan los primeros tres corrimientos.
+		z(11 downto 3) := num_bin;
+		for i in 0 to 5 loop
+			-- Unidades (4 bits).
+			if z(12 downto 9) > 4 then
+				z(12 downto 9) := z(12 downto 9) + 3;
+			end if;
+			-- Decenas (4 bits).
+			if z(16 downto 13) > 4 then
+				z(16 downto 13) := z(16 downto 13) + 3;
+			end if;
+			-- Centenas (3 bits).
+			if z(19 downto 17) > 4 then
+				z(19 downto 17) := z(19 downto 17) + 3;
+			end if;
+			-- Corrimiento a la izquierda.
+			z(19 downto 1) := z(18 downto 0);
+		end loop;
+		-- Pasando datos de variable Z, correspondiente a BCD.
+		num_bcd <= z(19 downto 9);
+	end process;
+	--Process #n...
+end solve;
