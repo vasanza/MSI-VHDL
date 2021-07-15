7d2471adf2f684f97b0d1472e00eb5ba11c60126
vasanza <vasanza@gmail.com>
2021-07-15 13:41:07 -0500

    Search max value example SD2

---
+++ cnt_Ld.vhd
@@ -0,0 +1,37 @@
+--Counter up
+--https://vasanza.blogspot.com
+
+--Library
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+--Entity
+entity cnt_Ld is
+	generic ( n: integer :=4);--<------- nbits
+	port(
+		Clk,resetn,en,ld: in std_logic;
+		D: in std_logic_vector(n-1 downto 0);
+		q: buffer std_logic_vector(n-1 downto 0));
+end cnt_Ld;
+
+--Architecture
+architecture solve of cnt_Ld is
+	-- Signals,Constants,Variables,Components
+	begin
+	--Process #1
+	process(resetn,clk)
+	--Sequential programming
+		begin
+			if resetn='0' then
+				q<=(others => '0');
+			elsif clk'event and clk='1' then
+				if en='1' and ld='0' then
+					q<=q+1;
+				elsif en='1' and ld='1' then
+					Q<= D;
+				end if;
+			end if;
+	end process;
+	--Process #n...
+end solve;
