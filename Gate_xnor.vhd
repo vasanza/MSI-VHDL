115719ce077552e8701d9d0bb5f3557804cf9a76
vasanza <vasanza@gmail.com>
2021-08-26 11:37:31 -0500

    Update and rename puerta_xnor.vhd to Gate_xnor.vhd

--- Gate_xnor.vhd
+++ Gate_xnor.vhd
@@ -1,20 +1,21 @@
 --N-bit single port xnor
 --https://vasanza.blogspot.com
+--https://github.com/vasanza/MSI-VHDL
 
 --Library
 library ieee;
 use ieee.std_logic_1164.all;
 
 --Entity
-entity puerta_xnor is
+entity Gate_xnor is
 	generic ( n: integer :=8);--<-- nbits
 	port(
 		A,B: in std_logic_vector(n-1 downto 0);
 		F: out std_logic_vector(n-1 downto 0));
-end puerta_xnor;
+end Gate_xnor;
 
 --Architecture
-architecture solve of puerta_xnor is
+architecture solve of Gate_xnor is
 	-- Signals,Constants,Variables,Components
 	begin
 		F<= A xnor B;
