a448cd481398a09bb3606c22c8f60f0f4c71bc2b
vasanza <vasanza@gmail.com>
2021-08-26 11:36:57 -0500

    Update and rename puerta_not.vhd to Gate_not.vhd

--- Gate_not.vhd
+++ Gate_not.vhd
@@ -1,20 +1,21 @@
 --N-bit single port inverter
 --https://vasanza.blogspot.com
+--https://github.com/vasanza/MSI-VHDL
 
 --Library
 library ieee;
 use ieee.std_logic_1164.all;
 
 --Entity
-entity puerta_not is
+entity Gate_not is
 	generic ( n: integer :=1);--<-- nbits
 	port(
 		X: in std_logic_vector(n-1 downto 0);
 		S: out std_logic_vector(n-1 downto 0));
-end puerta_not;
+end Gate_not;
 
 --Architecture
-architecture solve of puerta_not is
+architecture solve of Gate_not is
 	-- Signals,Constants,Variables,Components
 	begin
 		s<=not(X);
