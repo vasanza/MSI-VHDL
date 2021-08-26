26ffd3f5fcf2499562fdb989cc0819ce84bc6053
vasanza <vasanza@gmail.com>
2021-08-26 11:08:44 -0500

    Update and rename DecoBCD_7seg.vhd to decoBCD_7seg.vhd

--- decoBCD_7seg.vhd
+++ decoBCD_7seg.vhd
@@ -6,14 +6,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 --Entity
-entity DecoBCD_7seg is
+entity decoBCD_7seg is
 	port(
 		BCD: in std_logic_vector(3 downto 0);
 		anodo7: out std_logic_vector(6 downto 0));
-end DecoBCD_7seg;
+end decoBCD_7seg;
 
 --Architecture
-architecture solve of DecoBCD_7seg is
+architecture solve of decoBCD_7seg is
 	-- Signals,Constants,Variables,Components
 	begin
 	--Process #1
