8089cd2a2fc9256339ac094f867dd0a3b0e655e1
vasanza <vasanza@gmail.com>
2021-08-26 11:34:22 -0500

    Update and rename puerta_xor.vhd to Gate_xor.vhd

--- Gate_xor.vhd
+++ Gate_xor.vhd
@@ -1,20 +1,21 @@
---N-bit single port xor
+--N-bit single xor gate
 --https://vasanza.blogspot.com
+--https://github.com/vasanza/MSI-VHDL
 
 --Library
 library ieee;
 use ieee.std_logic_1164.all;
 
 --Entity
-entity puerta_xor is
+entity Gate_xor is
 	generic ( n: integer :=8);--<-- nbits
 	port(
 		A,B: in std_logic_vector(n-1 downto 0);
 		F: out std_logic_vector(n-1 downto 0));
-end puerta_xor;
+end Gate_xor;
 
 --Architecture
-architecture solve of puerta_xor is
+architecture solve of Gate_xor is
 	-- Signals,Constants,Variables,Components
 	begin
 		F<= A xor B;
