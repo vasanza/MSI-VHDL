eae1177274eaa288a862ce8e2abe6e371f757369
vasanza <vasanza@gmail.com>
2021-08-26 11:28:32 -0500

    Update and rename buffer3s.vhd to Buffer_3s.vhd

--- Buffer_3s.vhd
+++ Buffer_3s.vhd
@@ -1,21 +1,22 @@
 --Three-state buffer
 --https://vasanza.blogspot.com
+--https://github.com/vasanza/MSI-VHDL
 
 --Library
 library ieee;    
 use ieee.std_logic_1164.all;    
 
 --Entity
-entity buffer3s is  
+entity Buffer_3s is  
 	generic(n: integer:=4);--<------- nbits   
 	port(
 		ent: in std_logic_vector(n-1 downto 0);    
       en: in std_logic;    
       sal: out std_logic_vector(n-1 downto 0));    
-end buffer3s;
+end Buffer_3s;
 
 --Architecture   
-architecture solve of buffer3s is
+architecture solve of Buffer_3s is
 	-- Signals,Constants,Variables,Components
 	begin    
 		sal<=(others=>'Z') when en='0' else ent;    
