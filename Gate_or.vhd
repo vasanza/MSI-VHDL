8dc0a93dcdf6af2bcc3112c142729ca70a5c1cb8
vasanza <vasanza@gmail.com>
2021-08-26 11:36:15 -0500

    Update and rename puerta_or.vhd to Gate_or.vhd

--- Gate_or.vhd
+++ Gate_or.vhd
@@ -1,20 +1,21 @@
---N-bit single port and
+--N-bit single and gate
 --https://vasanza.blogspot.com
+--https://github.com/vasanza/MSI-VHDL
 
 --Library
 library ieee;
 use ieee.std_logic_1164.all;
 
 --Entity
-entity puerta_or is
+entity Gate_or is
 	generic ( n: integer :=1);--<-- nbits
 	port(
 		A,B: in std_logic_vector(n-1 downto 0);
 		F: out std_logic_vector(n-1 downto 0));
-end puerta_or;
+end Gate_or;
 
 --Architecture
-architecture solve of puerta_or is
+architecture solve of Gate_or is
 	-- Signals,Constants,Variables,Components
 	begin
 		F<= A or B;
