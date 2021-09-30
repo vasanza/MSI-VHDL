a059c8b997b82bc4bb60002d79efd26077a4c548
vasanza <vasanza@gmail.com>
2021-09-30 12:05:27 -0500

    Update Reg_LR.vhd

--- Reg_LR.vhd
+++ Reg_LR.vhd
@@ -1,6 +1,6 @@
 --Left to Right Shift Register
---https://vasanza.blogspot.com
---https://github.com/vasanza/MSI-VHDL
+--https://github.com/VHDL-Digital-Systems
+--http://blog.espol.edu.ec/sistemasdigitalesfiec
 
 --Library
 library ieee;
