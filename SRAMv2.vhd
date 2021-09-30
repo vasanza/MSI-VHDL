c4f9bbfcffdf731c8790a6e4bbc81557d00dc23f
vasanza <vasanza@gmail.com>
2021-09-30 12:06:35 -0500

    Update SRAMv2.vhd

--- SRAMv2.vhd
+++ SRAMv2.vhd
@@ -1,6 +1,6 @@
 --32-word 8-bit synchronous RAM
---https://vasanza.blogspot.com
---https://github.com/vasanza/MSI-VHDL
+--https://github.com/VHDL-Digital-Systems
+--http://blog.espol.edu.ec/sistemasdigitalesfiec
 -- By: Bryan macias and Angel Villamar (2021PAO1)
 
 --Library
