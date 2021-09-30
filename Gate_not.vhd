91f85e9871f923745ee8c5c6a1151c8346f5e135
vasanza <vasanza@gmail.com>
2021-09-30 12:02:46 -0500

    Update Gate_not.vhd

--- Gate_not.vhd
+++ Gate_not.vhd
@@ -1,6 +1,6 @@
 --N-bit single port inverter
---https://vasanza.blogspot.com
---https://github.com/vasanza/MSI-VHDL
+--https://github.com/VHDL-Digital-Systems
+--http://blog.espol.edu.ec/sistemasdigitalesfiec
 
 --Library
 library ieee;
