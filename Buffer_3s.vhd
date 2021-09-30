c5c792e31ac5cb548ee9318ec0282266956da01a
vasanza <vasanza@gmail.com>
2021-09-30 11:57:33 -0500

    Update Buffer_3s.vhd

--- Buffer_3s.vhd
+++ Buffer_3s.vhd
@@ -1,6 +1,6 @@
 --Three-state buffer
---https://vasanza.blogspot.com
---https://github.com/vasanza/MSI-VHDL
+--https://github.com/VHDL-Digital-Systems
+--http://blog.espol.edu.ec/sistemasdigitalesfiec
 
 --Library
 library ieee;    
